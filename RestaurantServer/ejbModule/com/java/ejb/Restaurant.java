18acd007fe4185d9dffdce6552b6a1fd4d1bde7b
pramitad <pramitad@ICS-LT-969T8C3.mshome.net>
2024-01-25 10:41:07 +0530

    Ejb Project Created only

---
+++ RestaurantServer/ejbModule/com/java/ejb/Restaurant.java
@@ -0,0 +1,19 @@
+package com.java.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Restaurant
+ */
+@Stateless
+
+public class Restaurant implements RestaurantRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public Restaurant() {
+        // TODO Auto-generated constructor stub
+    }
+}
